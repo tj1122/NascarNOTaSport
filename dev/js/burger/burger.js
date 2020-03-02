8b5aa3250abf17a8f47cc9c924454d9663d628d2
tj1122 <tom.falzani@gmail.com>
2020-03-02 18:29:15 -0500

    uploading base files
    - global header and footer
    - color variables in _variable.scss
    - organized file structure

---
+++ dev/js/burger/burger.js
@@ -0,0 +1,10 @@
+// console.log("burger js file");
+
+/* -------------------------------
+    Burger Maintenance
+------------------------------ */
+
+// Hide the burger outline
+gsap.set("#outline",{alpha:0});
+// change the transformOrigin of the 2 lines
+gsap.set(".lines",{transformOrigin: "50% 50%"});
---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+// @codekit-prepend "navigation.js";
+
+// @codekit-prepend "burger/burger.js";
+// @codekit-prepend "burger/burger-mouseenter.js";
+// @codekit-prepend "burger/burger-mouseleave.js";
+// @codekit-prepend "burger/burger-click.js";
+
+$( document ).ready(function() {
+    gsap.registerPlugin(DrawSVGPlugin);
+});
