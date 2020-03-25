d52cb3efcd217a57dbe62096b19e3751dc8122c9
Andrew Johnson <akj55@drexel.edu>
2020-03-25 13:49:13 -0500

    Burger Animation
    Animation for Burger customized.

--- dev/js/burger/burger.js
+++ dev/js/burger/burger.js
@@ -8,3 +8,7 @@
 // gsap.set("#outline",{alpha:0});
 // change the transformOrigin of the 2 lines
 // gsap.set(".lines",{transformOrigin: "50% 50%"});
+gsap.set(".cross",{drawSVG:0,transformOrigin: "50% 50%",alpha:0});
+gsap.set("#down-left",{rotate:180});
+gsap.set("#up-left",{rotate:180});
+gsap.set("#circle-outline",{drawSVG:0, rotate:90,transformOrigin: "50% 50%"});
